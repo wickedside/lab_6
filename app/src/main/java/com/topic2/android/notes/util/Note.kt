e91af089c46705cf6b6117129061504167634159
wickedside <w1ck3ds1d3@gmail.com>
2023-05-23 10:37:16 +0300

    added Note.kt

---
+++ app/src/main/java/com/topic2/android/notes/util/Note.kt
@@ -0,0 +1,32 @@
+package com.raywenderlich.android.jetnotes.util.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Checkbox
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.topic2.android.notes.theme.rwGreen
+
+@Composable
+fun Note(){
+    Box(
+        modifier = Modifier
+            .size(40.dp)
+            .background(rwGreen)
+    )
+    Text(text = "Заголовок", maxLines = 1)
+    Text(text = "Содержимое", maxLines = 1)
+    Checkbox(
+        checked = false, onCheckedChange = { },
+        modifier = Modifier.padding(start = 8.dp)
+    )
+}
+
+@Preview
+@Composable
+fun NotePreview(){
+    Note()
+}
