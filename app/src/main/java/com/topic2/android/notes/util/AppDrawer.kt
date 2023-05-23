d4258ce57fa3d178e812dcb93ea507794bc071a2
wickedside <w1ck3ds1d3@gmail.com>
2023-05-23 11:17:31 +0300

    edited AppDrawer.kt

--- app/src/main/java/com/topic2/android/notes/util/AppDrawer.kt
+++ app/src/main/java/com/topic2/android/notes/util/AppDrawer.kt
@@ -3,11 +3,10 @@ package com.raywenderlich.android.jetnotes.util.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Switch
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.R
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
@@ -17,8 +16,10 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.topic2.android.notes.routing.Screen
 import com.topic2.android.notes.theme.JetNotesTheme
 import com.topic2.android.notes.theme.JetNotesThemeSettings
+import com.topic2.android.notes.routing.NotesRouter
 
 @Composable
 private fun AppDrawerHeader(){
@@ -136,5 +137,54 @@ private fun LightDarkThemeItem(){
                 .padding(start = 8.dp, end = 8.dp)
                 .align(alignment = Alignment.CenterVertically)
         )
+    }
+}
+
+@Composable
+private fun LightDarkThemeItemPreview(){
+    JetNotesTheme {
+        LightDarkThemeItem()
+    }
+}
+
+@Composable
+fun AppDrawer(
+    currentScreen: Screen,
+    closeDrawerAction: () ->Unit
+) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        AppDrawerHeader()
+
+        Divider(color = MaterialTheme.colors.onSurface.copy(alpha = .2f))
+
+        ScreenNavigationButton(
+            icon = Icons.Filled.Home,
+            label = "Заметки",
+            isSelected = currentScreen == Screen.Notes,
+            onClick = {
+                NotesRouter.navigateTo(Screen.Notes)
+                closeDrawerAction()
+            }
+        )
+        ScreenNavigationButton(
+            icon = Icons.Filled.Delete,
+            label = "Корзина",
+            isSelected = currentScreen == Screen.Trash,
+            onClick = {
+                NotesRouter.navigateTo(Screen.Trash)
+                closeDrawerAction
+            }
+        )
+        LightDarkThemeItem()
+    }
+}
+
+@Preview
+@Composable
+fun AppDrawerPreview(){
+    JetNotesTheme {
+        AppDrawer(Screen.Notes){
+
+        }
     }
 }
